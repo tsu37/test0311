a3e00d0e566d175780d880ce8e2e54556f505c7d
Tsutomu Kurafuji <bull.bull.bull91@gmail.com>
2017-07-19 11:58:10 +0000

    list5.46_test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -11,5 +10,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     get contact_path
     assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
