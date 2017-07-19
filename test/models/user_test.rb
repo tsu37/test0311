b4d0f07119e2728e3ffb8c43b16d1c958ab28dbd
Tsutomu Kurafuji <bull.bull.bull91@gmail.com>
2017-07-19 13:36:09 +0000

    list_6.17_test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,13 +10,13 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
-  test "name should be present" do
-    @user.name = ""
+  test "name should not be too long" do
+    @user.name = "a" * 51
     assert_not @user.valid?
   end
 
-  test "email should be present" do
-    @user.email = "     "
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
 end
