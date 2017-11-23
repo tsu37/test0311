22be2d97992cb196efab28f5602bd227cd876384
Tsutomu Kurafuji <bull.bull.bull91@gmail.com>
2017-11-23 01:02:30 +0000

    dbカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805091133) do
+ActiveRecord::Schema.define(version: 20171123003702) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170805091133) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "picture"
+    t.string   "time"
+    t.string   "date"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
