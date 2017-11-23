0f43c1f4a8eb1e7a87fd09a8ec56fc6165f399f4
Tsutomu Kurafuji <bull.bull.bull91@gmail.com>
2017-11-23 07:02:25 +0000

    herokuセットアップなど

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -5,10 +5,10 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:success] = ""
+      flash[:success] = "アカウントが有効になりました。"
       redirect_to user
     else
-      flash[:danger] = ""
+      flash[:danger] = "リンクが無効です"
       redirect_to root_url
     end
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -70,7 +70,7 @@ class UserTest < ActiveSupport::TestCase
   test "associated microposts should be destroyed" do
     @user.save
     @user.microposts.create!(content: "Lorem ipsum")
-    assert_difference 'Micropost.count', -1 do
+    assert_difference 'Todo.count', -1 do
       @user.destroy
     end
   end
